38eccf322bf49822b7b2a03a1539b71aa93b841b
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2024-12-06 00:56:52 +0600

    fix: solve card component props type error

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 type CardProps = {
   title: string;
-  description: string[];
-  image: string;
+  description: string;
+  image: StaticImageData;
 };
 function Card({ title, description, image }: CardProps) {
   return (
